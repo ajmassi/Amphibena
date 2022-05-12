70a7f4857c19a4c6dc1a3e2ff2bbbec55ee349aa
ajmassi <alexjmassi@gmail.com>
2022-05-11 23:07:04 -0400

    Convert packet_processing to object

--- amphivena/packet_processing.py
+++ amphivena/packet_processing.py
@@ -9,48 +9,50 @@ from scapy.layers.tls.record import TLS
 log = logging.getLogger(__name__)
 
 
-def proc_start():
-    p = multiprocessing.Process(target=run)
-    p.start()
-    return p
+class PacketProcessor:
+    def __init__(self, config):
+        self._config = config
+        self.proc = None
 
+    def start(self):
+        self.proc = multiprocessing.Process(target=self.examine_packets)
+        self.proc.start()
+        return self.proc
 
-def proc_stop(p):
-    # TODO: This works but is sketchy, should consider better means of controlling the process
-    if p._closed is False:
-        p.terminate()
-        p.join()
-        p.close()
+    def stop(self):
+        # TODO: This works but is sketchy, should consider better means of controlling the process
+        if self.proc._closed is False:
+            self.proc.terminate()
+            self.proc.join()
+            self.proc.close()
 
+    @staticmethod
+    def finalize(pkt):
+        # Wrapped for mocking during tests - cdef functions are un-mockable
+        pkt.accept()
 
-def accept(pkt):
-    # Wrapped for mocking during tests - cdef functions are un-mockable
-    pkt.accept()
+    def manipulate(self, pkt):
+        scapy_packet = Ether(bytes(pkt.get_payload()))
 
+        # Basic PoC processing attempt
+        if (
+            scapy_packet.haslayer(TLS)
+            and type(scapy_packet.getlayer(TLS).msg[0])
+            is scapy.layers.tls.handshake.TLSClientHello
+        ):
+            scapy_packet.getlayer(TLS).version = 0x0304
 
-def print_and_accept(pkt):
-    scapy_packet = Ether(bytes(pkt.get_payload()))
+        pkt.set_payload(scapy_packet.build())
+        self.finalize(pkt)
 
-    # Basic PoC processing attempt
-    if (
-        scapy_packet.haslayer(TLS)
-        and type(scapy_packet.getlayer(TLS).msg[0])
-        is scapy.layers.tls.handshake.TLSClientHello
-    ):
-        scapy_packet.getlayer(TLS).version = 0x0304
+    def examine_packets(self):
+        nfqueue = netfilterqueue.NetfilterQueue()
+        nfqueue.bind(1, self.manipulate)
+        try:
+            print("starting nfqueue")
+            log.info("starting nfqueue")
+            nfqueue.run()
+        except KeyboardInterrupt:
+            print("shutting down nfqueue")
 
-    pkt.set_payload(scapy_packet.build())
-    accept(pkt)
-
-
-def run():
-    nfqueue = netfilterqueue.NetfilterQueue()
-    nfqueue.bind(1, print_and_accept)
-    try:
-        print("starting nfqueue")
-        log.info("starting nfqueue")
-        nfqueue.run()
-    except KeyboardInterrupt:
-        print("shutting down nfqueue")
-
-    nfqueue.unbind()
+        nfqueue.unbind()
